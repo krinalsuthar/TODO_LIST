1d85e7108a56c3d1dcbd4cba31d71e228d6f9ff1
krinalsuthar <141125453+krinalsuthar@users.noreply.github.com>
2023-10-28 22:43:46 +0530

    TODO_LIST

---
+++ app.js
@@ -0,0 +1,28 @@
+const inputbox = document.getElementById("input-box");
+const list = document.getElementById("ul");
+
+function addTask() {
+  if (inputbox.value === "") {
+    alert("you must be write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputbox.value = "";
+}
+
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
